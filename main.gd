9a52b6bc2868107b8bc0e1e41160ebc8febc226e
Shoaib Ansari <shousa786@gmail.com>
2025-10-20 18:19:38 +0530

    Added the Project files. No Assets Included.
    Assets can be found Godot's Repository

---
+++ main.gd
@@ -0,0 +1,23 @@
+extends Node
+@export var mob_scene : PackedScene
+
+func _ready() -> void:
+	$UI/Retry.hide()
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocations")
+	mob_spawn_location.progress_ratio = randf()
+	var player_position = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	add_child(mob)
+	mob.squashed.connect($UI/ScoreLabel._on_mob_squashed.bind())
+
+func _on_player_hit() -> void:
+	$MobTimer.stop()
+	$UI/Retry.show()
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and $UI/Retry.visible:
+		get_tree().reload_current_scene()
